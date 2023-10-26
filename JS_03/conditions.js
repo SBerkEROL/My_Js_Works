a3ec251eb3fe3a9718b7ba48e985e110b28ff2d3
SBerkEROL <berkerol0@gmail.com>
2023-10-26 22:07:58 +0200

    conditions

--- JS_03/conditions.js
+++ JS_03/conditions.js
@@ -35,3 +35,84 @@ if (not < 0 || not > 100) {
 if (harf_notu) {
   console.log("Sonuc:", harf_notu);
 }
+
+//! 3 tam sayidan en buyugu bulan algoritma
+
+const s1 = 1;
+const s2 = 2;
+const s3 = 5;
+
+// //* YONTEM - 1
+if (s1 >= s2 && s1 >= s3) {
+  console.log("En büyük sayi:", s1);
+} else if (s2 >= s1 && s2 >= s3) {
+  console.log("En büyük sayi:", s2);
+} else if (s3 >= s1 && s3 >= s2) {
+  console.log("En büyük sayi:", s3);
+}
+
+//* YONTEM - 2 (olmadi)
+// nasted if (ic ice if)
+
+const say1 = 1;
+const say2 = 2;
+const say3 = 5;
+if (say1 >= say2) {
+  if (say1 >= say3) {
+    console.log("En büyük:", say1);
+  }
+}
+if (say2 >= say1) {
+  if (say2 >= say3) {
+    console.log("En büyük:", say2);
+  }
+}
+if (say3 >= say1) {
+  if (say3 >= say2) {
+    console.log("En büyük:", say3);
+  }
+}
+
+if (s1 >= s2) {
+  if (s1 >= s3) {
+    console.log("En buyuk", s1);
+  }
+} else if (s2 >= s1) {
+  if (s2 >= s3) {
+    console.log("En buyuk", s2);
+  }
+} else if (s3 >= s1) {
+  if (s3 >= s2) {
+    console.log("En buyuk", n3);
+  }
+}
+
+//*********** TERNARY **************//
+
+//! 1. YONTEM
+// not1 diye degisken olusturduk ona girilen degere gore gecti mi kaldi mi hespalanacak
+const not1 = 70;
+
+// not1'e girilen deger 50'den buyuk ya da esitse "Gecti" yazdirsin degilse  "kaldi yazdirsin"
+const sonuc = not1 >= 50 ? "Gecti" : "Kaldi";
+console.log(sonuc);
+
+//****
+let derstenGecen = 0;
+let derstenKalan = 0;
+
+// yukaridan aldigimiz not1'i kullandik
+not1 >= 50 ? (derstenGecen = derstenGecen + 1) : derstenKalan++;
+
+console.log({ derstenGecen });
+console.log({ derstenKalan });
+
+//! 5 yildan fazla calisanlara %10 zam
+//? tek kosullu ternary - if() - tek basina if gibi
+
+const calisma_Yili = 5;
+let maas = 15000;
+
+// calisma yili 5'e esit ya da buyukse maas * 1.1(%10)'u maasa ata
+calisma_Yili >= 5 ? (maas *= 1.1) : "";
+console.log(maas);
