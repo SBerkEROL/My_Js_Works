194bf8a9fbe717188d12bc0a6ce52ed0b7ff7054
SBerkEROL <berkerol0@gmail.com>
2023-10-30 21:08:59 +0100

    JS-05_bitti

--- JS-05_functions/declaration.js
+++ JS-05_functions/declaration.js
@@ -73,3 +73,54 @@ function yazdir3(lang, name = "isimsiz") {
 yazdir3("Halo", "Mert");
 yazdir3("hi", "Alp");
 yazdir3("Bilge");
+
+console.log("-------");
+
+//? Return (veri donduren) fonksiyonlar
+
+//! ornek1 - cozum 1
+// islemin sonucunu asagidaki kodlarda kullanacaksan return kullanmamiz gerekir
+function bol(a, b) {
+  const result = a / b;
+  return result; //! return gorunce fonksiyondan cikar
+}
+
+let sonuc = bol(10, 2);
+console.log("Bolme sonucu", sonuc);
+
+console.log("-------");
+
+//! ornek1 - cozum 2
+function bol(a, b) {
+  return a / b;
+}
+
+let sonuc2 = bol(10, 2);
+console.log("Bolme sonucu", sonuc);
+
+//! ornek2 - toplama
+
+function toplama(n1, n2, n3) {
+  return n1 + n2 + n3;
+}
+
+console.log(`islem sonucu:${toplama(3, 5, 7)}`);
+
+//! tek-cift hesaplama
+//? console'dan girilen bir sayinin tek mi cift mi oldugunu hesaplayan ve sonucu ana programa donduren kodu fonksiyonlar ile yazan kod
+
+function tekCift(sayi) {
+  return sayi % 2 === 0 ? "cift" : "tek";
+}
+const sayi = Number(prompt("Lutfen Bir sayi giriniz"));
+console.log(`${sayi} ${tekCift(sayi)} sayidir`);
+console.log("----------");
+//? 1. yontemdeki fonksyonu konumu
+
+yaz();
+//? hoisted fonksiyonu (kod en yukarı tasiniyor ve her yerden cagirilabilir)
+function yaz() {
+  console.log("Merhaba");
+}
+yaz();
+yaz();
